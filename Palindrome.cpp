9de5db80ff3e16615acb8f9fdda4bff315cb8103
Andrew Thomas <45573723+ElectricOneStar@users.noreply.github.com>
2019-10-06 09:57:12 -0700

    Add files via upload

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -1,5 +1,5 @@
 // Palindrome by Andrew Thomas. This program will read in an input and process that input(take out the spaces, make it all the same case, and remove any punctuation) to see if it is a palindrome or not(output)
-#include <iostream> // gets the libraries
+#include <iostream> // gets the libraries 
 #include <cstring>
 using namespace std;
 
@@ -15,7 +15,7 @@ int main(){ // main block of code
   int difference = 0;
   // do{ // do while loop that ensures that the input is at most 80 charecters long.
     cout << "please enter a sentence that is at most 80 charecters long (spaces are a charecter) and this program will say wheather or not it is a palindrome\n";
-
+    
     cin.get(str, 80); // gets a cstring
   cin.get();
 
@@ -27,15 +27,14 @@ int main(){ // main block of code
   // while(len>80);
   //cout << len << endl;
   for(int j = 0; j < len; j++){ // for loop that processes the initial input
-    alphabet = str[j];
+    alphabet = str[j]; 
    difference = 0;
    if(alphabet >= 65 && alphabet <= 90){ // checks if the case is upper with ascii
      difference = alphabet + 32;
      str3[count2] = char(difference); // if it is convert it to lower case
-     count2++;
+     count2++; 
    }
-   if(str[j] != '\0' && str[j] != ' ' && str[j] != ',' && str[j] != ':' && str[j] != ';' && str[j] != '.' && str[j] != '\"' && str[j] != '\'' && str[j] != '?' && str[j] != '!' && alphabet < 65 || alphabet > 90\
-){// checks if lowercase and is not a space or punction
+   if(str[j] != '\0' && str[j] != ' ' && str[j] != ',' && str[j] != ':' && str[j] != ';' && str[j] != '.' && str[j] != '\"' && str[j] != '\'' && str[j] != '?' && str[j] != '!' && alphabet < 65 || alphabet > 90){// checks if lowercase and is not a space or punction
      str3[count2] = str[j]; // if it is put it into the procces input
       count2++;
     }
@@ -46,8 +45,8 @@ int main(){ // main block of code
   len2 = strlen(str3); // gets length of processed input
   for(int i = len2 - 1; i >= 0; i--){ // goes through the procced input and reverses it
       if(str[i] != '\0'){
-        str2[count] = str3[i];
-        count++;
+	str2[count] = str3[i]; 
+	count++;
       }
     }
   str2[len2] = '\0'; // puts null terminator charecter at the end
